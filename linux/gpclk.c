34cc92adcca03dd8a98facd4598bb899ece43bc8
Michael Davidsaver <mdavidsaver@gmail.com>
2017-09-18 10:37:13 -0500

    more linux

--- linux/gpclk.c
+++ linux/gpclk.c
@@ -1,12 +1,16 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
+#include <linux/of_platform.h>
+#include <linux/platform_device.h>
+#include <linux/clk.h>
 #include <linux/io.h>
 
 static
 int bcm2835_gpclk_init(void)
 {
     struct device_node *node;
+    struct platform_device *pdev;
     struct resource res;
 
     node = of_find_node_by_name(NULL, "cprman");
@@ -15,12 +19,36 @@ int bcm2835_gpclk_init(void)
         return -ENODEV;
     }
 
+    if((pdev=of_find_device_by_node(node))!=NULL) {
+        dev_info(&pdev->dev, "Found cprman");
+
+
+    }
+
     if(of_address_to_resource(node, 0, &res)) {
         printk(KERN_ERR "No cprman resource0\n");
         return -ENODEV;
     }
 
-    printk("Found cprman @%llx\n", (unsigned long long)res.start);
+    printk(KERN_INFO "Found cprman @%llx\n", (unsigned long long)res.start);
+
+    {
+        int clkidx = 0;
+        while(1) {
+            struct clk *C;
+            unsigned long rate;
+            C = of_clk_get(node, clkidx);
+            if(IS_ERR(C))
+                break;
+
+            rate = clk_get_rate(C);
+
+            printk(KERN_INFO "cprman clock%d rate=%lu\n", clkidx, rate);
+
+            clk_put(C);
+            clkidx++;
+        }
+    }
 
     return 0;
 }
